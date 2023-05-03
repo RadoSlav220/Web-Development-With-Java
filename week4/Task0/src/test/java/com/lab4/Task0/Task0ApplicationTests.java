303077e6b4f59fa4594ece9ab767e9e9cc6d95ba
RadoSlav <radoslav4096@gmail.com>
2023-05-03 12:00:58 +0300

    Create project from task0 lab4

---
+++ week4/Task0/src/test/java/com/lab4/Task0/Task0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab4.Task0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
