515666fc2c59070fb0cd1afc7d62bb59aeb50aca
RadoSlav <radoslav4096@gmail.com>
2023-05-03 12:24:44 +0300

    Create Book class

--- week4/Task0/src/main/java/com/lab4/Task0/Task0Application.java
+++ week4/Task0/src/main/java/com/lab4/Task0/Task0Application.java
@@ -1,13 +1,26 @@
 package com.lab4.Task0;
 
+import com.lab4.Task0.model.Book;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
-public class Task0Application {
+public class Task0Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task0Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Book book = new Book("Narnia", "John",
+								new BigDecimal(12.09), "Pulblisher",
+								LocalDate.of(2001, 12, 1));
+		
+	}
 }
---
+++ week4/Task0/src/main/java/com/lab4/Task0/model/Book.java
@@ -0,0 +1,18 @@
+package com.lab4.Task0.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private BigDecimal price;
+    private String publisher;
+    private LocalDate publishedYear;
+}
